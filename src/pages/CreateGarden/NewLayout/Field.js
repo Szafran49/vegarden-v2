7b86a62d43db24470464d50adda6f3cca891687f
Szafran49 <59666170+Szafran49@users.noreply.github.com>
2021-04-02 21:59:59 +0200

    U mad?

--- src/pages/CreateGarden/NewLayout/Field.js
+++ src/pages/CreateGarden/NewLayout/Field.js
@@ -1,143 +1,166 @@
-import { Container, Typography } from '@material-ui/core'
+import { Container, Typography } from "@material-ui/core";
 import IconButton from "../../../shared/StyledIconButton";
 import AddIcon from "@material-ui/icons/Add";
-import { useState, useEffect } from 'react'
-import { makeStyles } from '@material-ui/styles'
-import Vegetables from './Vegetables'
-import Column from './Column';
-import ColumnLayout from './ColumnLayout';
+import { useState, useEffect } from "react";
+import { makeStyles } from "@material-ui/styles";
+import Vegetables from "./Vegetables";
+import Column from "./Column";
+import ColumnLayout from "./ColumnLayout";
 
 const useStyles = makeStyles((theme) => ({
-    text: {
-        padding: theme.spacing(1),
-    },
-    rowsContainer: {
-        display: "flex",
-        minHeight: 400,
-        outline: "10px solid brown",
-        marginTop: 100,
-        width: 900,
-        backgroundColor: "white",
-    },
+  text: {
+    padding: theme.spacing(1),
+  },
+  rowsContainer: {
+    display: "flex",
+    minHeight: 400,
+    outline: "10px solid brown",
+    marginTop: 100,
+    width: 900,
+    backgroundColor: "white",
+  },
 
-    add: {
-        position: 'relative',
-        left: 0,
-        top: 0,
-        bottom: 0,
-        color: "black",
-        maxWidth: 40,
-        maxHeight: 40,
-        border: "1px solid black",
-    },
-
+  add: {
+    position: "relative",
+    left: 0,
+    top: 0,
+    bottom: 0,
+    color: "black",
+    maxWidth: 40,
+    maxHeight: 40,
+    border: "1px solid black",
+  },
+  remainingSpaceDescription: {
+    position: "relative ",
+    display: "flex",
+    height: 70,
+    top: -79,
+    justifyContent: "center",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  remainingSpace: {
+    display: "flex",
+    alignItems: "baseline",
+    justifyContent: "center",
+  },
 }));
 
-export default function Field({ selectedItems, setSelectedItems, fieldWidth, items }) {
-    const [remainingSpace, setRemainingSpace] = useState(fieldWidth)
-    const [isVegetablesVisible, setIsVegetablesVisible] = useState(false)
-    const [isVegetablesForEditVisible, setIsVegetablesForeEditVisible] = useState(false)
-    const [rowToChange, setRowToChange] = useState(null)
-    const classes = useStyles();
+export default function Field({
+  selectedItems,
+  setSelectedItems,
+  fieldWidth,
+  items,
+}) {
+  const [remainingSpace, setRemainingSpace] = useState(fieldWidth);
+  const [isVegetablesVisible, setIsVegetablesVisible] = useState(false);
+  const [isVegetablesForEditVisible, setIsVegetablesForEditVisible] = useState(
+    false
+  );
+  const [rowToChange, setRowToChange] = useState(null);
+  const classes = useStyles();
 
-    useEffect(function updateRemainingSpace() {
-        let sumWidths = 0;
-        selectedItems.map((item) => sumWidths += item.width)
-        sumWidths = fieldWidth - sumWidths
-        setRemainingSpace(sumWidths);
-    }, [selectedItems, fieldWidth])
+  useEffect(
+    function updateRemainingSpace() {
+      let sumWidths = 0;
+      selectedItems.map((item) => (sumWidths += item.width));
+      sumWidths = fieldWidth - sumWidths;
+      setRemainingSpace(sumWidths);
+    },
+    [selectedItems, fieldWidth]
+  );
 
-    function changeVegetablesDisplay() {
-        setIsVegetablesVisible(!isVegetablesVisible)
-    }
+  function changeVegetablesDisplay() {
+    setIsVegetablesVisible(!isVegetablesVisible);
+  }
 
-    function handleVegetableForEditDisplayChange(index) {
-        setRowToChange(index)
-        setIsVegetablesForeEditVisible(!isVegetablesForEditVisible)
-    }
+  function handleVegetableForEditDisplayChange(index) {
+    setRowToChange(index);
+    setIsVegetablesForEditVisible(!isVegetablesForEditVisible);
+  }
 
-    function addToSelectedItems(item) {
+  function addToSelectedItems(item) {
+    const defaultWidthValue = 10;
+    const newItem = (({ id, image }) => ({ id, image }))(item);
+    newItem.width = defaultWidthValue;
+    setSelectedItems((selectedItems) => [...selectedItems, newItem]);
+    changeVegetablesDisplay();
+  }
 
-        const defaultWidthValue = 10
-        const newItem = (({ id, image }) => ({ id, image }))(item)
-        newItem.width = defaultWidthValue
-        setSelectedItems((selectedItems) => [...selectedItems, newItem]);
-        changeVegetablesDisplay()
-    }
+  function handleVegetableChange(item, index, width) {
+    let data = [...selectedItems];
+    item.width = selectedItems[rowToChange].width;
+    data[index] = item;
+    setSelectedItems(data);
+    handleVegetableForEditDisplayChange(null);
+  }
 
-    function handleVegetableChange(item, index, width) {
-        let data = [...selectedItems]
-        item.width = width
-        data[index] = item
-        setSelectedItems(data)
-        handleVegetableForEditDisplayChange(null)
-    }
+  function handleVegetableDelete(index) {
+    const data = [...selectedItems];
+    data.splice(index, 1);
+    setSelectedItems(data);
+  }
 
-    function handleVegetableDelete(index) {
-        const data = [...selectedItems]
-        data.splice(index, 1)
-        setSelectedItems(data)
-    }
+  function handleVegetableWidthChange(index, desiredWidth) {
+    const width = parseInt(desiredWidth);
+    const data = [...selectedItems];
+    data[index].width = width;
+    setSelectedItems(data);
+  }
 
-    function handleVegetableWidthChange(index, desiredWidth) {
-        const width = parseInt(desiredWidth)
-        const data = [...selectedItems]
-        data[index].width = width
-        setSelectedItems(data)
+  return (
+    <Container maxWidth="md" align="center">
+      <div className={classes.rowsContainer}>
+        {selectedItems.map((item, index) => (
+          <ColumnLayout
+            width={item.width}
+            image={item.image}
+            id={item.id}
+            key={index}
+            fieldWidth={fieldWidth}
+          >
+            <Column
+              handleVegetableDelete={handleVegetableDelete}
+              handleVegetableForEditDisplayChange={
+                handleVegetableForEditDisplayChange
+              }
+              handleVegetableWidthChange={handleVegetableWidthChange}
+              index={index}
+              width={item.width}
+              fieldWidth={fieldWidth}
+            />
+            {isVegetablesForEditVisible ? (
+              <Vegetables
+                index={rowToChange}
+                items={items}
+                action={handleVegetableChange}
+              />
+            ) : null}
+          </ColumnLayout>
+        ))}
+        {remainingSpace > 0 ? (
+          <div className={classes.remainingSpaceDescription}>
+            <Typography>Pozostałe miejsce</Typography>
+            <div className={classes.remainingSpace}>
+              <Typography variant="h5">{remainingSpace}</Typography>
+              <Typography variant="h7">cm</Typography>
 
-    }
-
-    return (
-        <Container maxWidth="md" align="center">
-            <div className={classes.rowsContainer}>
-                {selectedItems.map((item, index) =>
-                    <ColumnLayout
-                        width={item.width}
-                        image={item.image}
-                        id={item.id}
-                        key={index}
-                        fieldWidth={fieldWidth}>
-                        <Column
-                            handleVegetableDelete={handleVegetableDelete}
-                            handleVegetableForEditDisplayChange={handleVegetableForEditDisplayChange}
-                            handleVegetableWidthChange={handleVegetableWidthChange}
-                            index={index}
-                            width={item.width}
-                            fieldWidth={fieldWidth}
-                        />
-                        {isVegetablesForEditVisible ?
-                            <Vegetables
-                                index={rowToChange}
-                                items={items}
-                                action={handleVegetableChange}
-                            />
-                            : null}
-                    </ColumnLayout>
-                )}
-                {remainingSpace > 0 ?
-                    <>
-                        <Typography variant='h7'>Pozostałe miejsce {remainingSpace}cm</Typography>
-                        <IconButton
-                            onClick={() => changeVegetablesDisplay()}
-                            className={classes.add}
-                        >
-                            <AddIcon />
-                        </IconButton>
-                    </>
-                    : null
-                }
-                {isVegetablesVisible ?
-                    <Vegetables
-                        items={items}
-                        action={addToSelectedItems}
-                    />
-                    : null
-                }
+              <IconButton
+                onClick={() => changeVegetablesDisplay()}
+                className={classes.add}
+              >
+                <AddIcon />
+              </IconButton>
             </div>
-            <Typography
-                className={classes.text}
-                variant="h5">Szerokość powierzchni - {fieldWidth}cm
-            </Typography>
-        </Container >
-    )
+          </div>
+        ) : null}
+        {isVegetablesVisible ? (
+          <Vegetables items={items} action={addToSelectedItems} />
+        ) : null}
+      </div>
+      <Typography className={classes.text} variant="h5">
+        Szerokość powierzchni - {fieldWidth}cm
+      </Typography>
+    </Container>
+  );
 }
--- src/pages/CreateGarden/NewLayout/Vegetables.js
+++ src/pages/CreateGarden/NewLayout/Vegetables.js
@@ -1,64 +1,63 @@
-import { GridList, GridListTile, GridListTileBar, Typography } from "@material-ui/core";
+import {
+  GridList,
+  GridListTile,
+  GridListTileBar,
+  Typography,
+} from "@material-ui/core";
 import IconButton from "../../../shared/StyledIconButton";
 import AddIcon from "@material-ui/icons/Add";
-import { makeStyles } from '@material-ui/styles'
+import { makeStyles } from "@material-ui/styles";
 
 const useStyles = makeStyles((theme) => ({
-    container: {
-        position: "absolute",
-        left: 0,
-        right: 0,
-        bottom: 0,
-        top: 90,
-        backgroundColor: "white",
-        zIndex: 9999,
-    },
-    gridList: {
-        width: 1000,
-        maxHeight: 550,
-    },
-    header: {
-        padding: theme.spacing(1)
-    },
-    barTitle: {
-        overflow: "visible",
-        whiteSpace: "normal",
-        textAlign: "center",
-        fontSize: "0.75rem",
-        height: 30,
-    },
+  container: {
+    position: "absolute",
+    left: 0,
+    right: 0,
+    bottom: 0,
+    top: 80,
+    backgroundColor: "white",
+    zIndex: 9999,
+  },
+  gridList: {
+    width: 1000,
+    maxHeight: 550,
+  },
+
+  header: {
+    padding: theme.spacing(1),
+  },
+
+  barTitle: {
+    overflow: "visible",
+    whiteSpace: "normal",
+    textAlign: "center",
+    fontSize: "0.75rem",
+    height: 30,
+  },
 }));
 
 export default function Vegetables({ items, action, index }) {
-    const classes = useStyles()
+  const classes = useStyles();
 
-    return (
-        <div className={classes.container}>
-            <Typography variant='h6'>Wybierz warzywo do zasadzenia</Typography>
-            <GridList className={classes.gridList} >
-                {items.map((item) => (
-                    <GridListTile
-                        key={item.image}
-                        cols={0.4}
-                        rows={1}
-                    >
-                        <img
-                            src={item.image}
-                            alt={item.id} />
-                        <GridListTileBar
-                            classes={{ title: classes.barTitle }}
-                            title={item.name}
-                            actionIcon={
-                                <IconButton
-                                    onClick={() => action(item, index)}
-                                >
-                                    <AddIcon />
-                                </IconButton>
-                            }
-                        />
-                    </GridListTile>
-                ))}
-            </GridList>
-        </div>
-    )
+  return (
+    <div className={classes.container}>
+      <Typography variant="h6">Wybierz warzywo do zasadzenia</Typography>
+      <GridList className={classes.gridList}>
+        {items.map((item) => (
+          <GridListTile key={item.image} cols={0.4} rows={1}>
+            <img src={item.image} alt={item.id} />
+            <GridListTileBar
+              classes={{ title: classes.barTitle }}
+              title={item.name}
+              actionIcon={
+                <IconButton onClick={() => action(item, index)}>
+                  <AddIcon />
+                </IconButton>
+              }
+            />
+          </GridListTile>
+        ))}
+      </GridList>
+    </div>
+  );
 }
